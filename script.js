981974cec52c2c5f6f63a0124266866bf5a2cb04
LunevaDariaa <daria.safri@gmail.com>
2023-11-02 14:07:45 -0400

    Created the logic of the game, paint, clear and change size buttons

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 const grid = document.querySelector(".grid");
+const clearBtn = document.querySelector(".clear_btn");
+const randomize = document.querySelector(".randomize");
+//create grid
+const gridSize = document.querySelector(".size_range");
 
-//create 4
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    grid.appendChild(cell);
+const createGrid = function () {
+  const size = gridSize.value;
+
+  grid.innerHTML = "";
+
+  for (let i = 0; i < gridSize.value; i++) {
+    for (let j = 0; j < gridSize.value; j++) {
+      const cell = document.createElement("div");
+      grid.style.gridTemplateColumns = `repeat(${gridSize.value},1fr)`;
+      grid.style.gridTemplateRows = `repeat(${gridSize.value},1fr)`;
+      cell.classList.add("cell");
+      grid.appendChild(cell);
+
+      randomize.addEventListener("click", function () {
+        const randomizer = Math.floor(Math.random(1, 255));
+        cell.style.backgroundColor = `rgb(${randomize}, ${randomize}, ${randomize})`;
+      });
+
+      cell.addEventListener("mouseover", function () {
+        cell.classList.add("painted");
+      });
+
+      clearBtn.addEventListener("click", function () {
+        cell.classList.remove("painted");
+        cell.classList.add("clearAll");
+      });
+    }
   }
-}
+};
+
+const gridItems = grid.querySelectorAll(".cell");
+
+createGrid();
+gridSize.addEventListener("input", createGrid);
+//[...gridItems] =
+//Create painting
+// const painting = function () {
+//   gridItems.forEach((gridItem) => {
+//     gridItem.addEventListener("mouseover", function () {
+//       gridItems.classList.add("painted");
+//     });
+//   });
+// };
+//Clear function
