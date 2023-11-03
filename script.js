033fdb419d1b9c0b4c5630641e481ff868a47287
LunevaDariaa <daria.safri@gmail.com>
2023-11-02 20:46:25 -0400

    Completed the project, assigned style and all functionality:erase, clear, black and randomize

--- script.js
+++ script.js
@@ -1,50 +1,66 @@
 const grid = document.querySelector(".grid");
 const clearBtn = document.querySelector(".clear_btn");
 const randomize = document.querySelector(".randomize");
-//create grid
+const blackBtn = document.querySelector(".black");
+const eraseBtn = document.querySelector(".eraser");
 const gridSize = document.querySelector(".size_range");
+
+const gridItems = [];
 
 const createGrid = function () {
   const size = gridSize.value;
-
   grid.innerHTML = "";
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr`;
 
-  for (let i = 0; i < gridSize.value; i++) {
-    for (let j = 0; j < gridSize.value; j++) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
       const cell = document.createElement("div");
-      grid.style.gridTemplateColumns = `repeat(${gridSize.value},1fr)`;
-      grid.style.gridTemplateRows = `repeat(${gridSize.value},1fr)`;
       cell.classList.add("cell");
       grid.appendChild(cell);
-
-      randomize.addEventListener("click", function () {
-        const randomizer = Math.floor(Math.random(1, 255));
-        cell.style.backgroundColor = `rgb(${randomize}, ${randomize}, ${randomize})`;
-      });
-
-      cell.addEventListener("mouseover", function () {
-        cell.classList.add("painted");
-      });
-
-      clearBtn.addEventListener("click", function () {
-        cell.classList.remove("painted");
-        cell.classList.add("clearAll");
-      });
+      gridItems.push(cell);
     }
   }
 };
 
-const gridItems = grid.querySelectorAll(".cell");
+const randomColor = function () {
+  gridItems.forEach((cell) => {
+    const randomeR = Math.round(Math.random() * 256);
+    const randomeG = Math.round(Math.random() * 256);
+    const randomeB = Math.round(Math.random() * 256);
+    cell.addEventListener("mouseover", function () {
+      cell.style.backgroundColor = `rgb(${randomeR}, ${randomeG}, ${randomeB})`;
+    });
+  });
+};
+
+const defaultColor = function () {
+  gridItems.forEach((cell) => {
+    cell.addEventListener("mouseover", function () {
+      cell.style.backgroundColor = "black";
+    });
+  });
+};
+
+const clearGrid = function () {
+  gridItems.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+    cell.classList.remove("painted");
+  });
+};
+
+const eraser = function () {
+  gridItems.forEach((cell) => {
+    cell.addEventListener("mouseover", function () {
+      cell.style.backgroundColor = "white";
+    });
+  });
+};
 
 createGrid();
 gridSize.addEventListener("input", createGrid);
-//[...gridItems] =
-//Create painting
-// const painting = function () {
-//   gridItems.forEach((gridItem) => {
-//     gridItem.addEventListener("mouseover", function () {
-//       gridItems.classList.add("painted");
-//     });
-//   });
-// };
-//Clear function
+
+randomize.addEventListener("click", randomColor);
+blackBtn.addEventListener("click", defaultColor);
+clearBtn.addEventListener("click", clearGrid);
+eraseBtn.addEventListener("click", eraser);
